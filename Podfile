d625b60e062cb6b04bb3247dee3b77af1a048ff3
Kamil Tustanowski <kamil.tustanowski@gmail.com>
2015-10-11 21:34:32 +0200

    Initial commit+.

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+
+target 'WeakItemContainer' do
+
+end
+
+target 'WeakItemContainerTests', :exclusive => true do
+    use_frameworks!
+    pod 'Nimble', '~> 2.0.0'
+end
+
--- WeakItemContainerTests/WeakItemContainerTests.swift
+++ WeakItemContainerTests/WeakItemContainerTests.swift
@@ -1,36 +1,104 @@
 //
 //  WeakItemContainerTests.swift
-//  WeakItemContainerTests
+//  PlayPlex
 //
-//  Created by Kamil Tustanowski on 08.10.2015.
-//  Copyright © 2015 Kamil Tustanowski. All rights reserved.
+//  Created by Kamil Tustanowski on 07.10.2015.
+//  Copyright © 2015 Viacom. All rights reserved.
 //
 
 import XCTest
+import Nimble
 @testable import WeakItemContainer
 
 class WeakItemContainerTests: XCTestCase {
     
+    var container: WeakItemContainer<NSObject>!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.container = WeakItemContainer<NSObject>()
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func testIfCanAppendItem() {
+        let object = NSObject()
+        
+        container.append(object)
+        
+        guard let objectFromContainer = container.weakItemsTable.allObjects.last else {
+            fail()
+            return
         }
+        
+        expect(objectFromContainer) === object
+    }
+    
+    func testIfCantHaveDuplicatedObjects() {
+        let object = NSObject()
+        container.append(object)
+        container.append(object)
+        
+        expect(self.container.weakItemsTable.count).to(equal(1))
+    }
+    
+    func testIfCanRemoveItem() {
+        let object = NSObject()
+        container.append(object)
+        
+        container.remove(object)
+        
+        expect(self.container.weakItemsTable.count).to(equal(0))
+    }
+    
+    func testIfCanRemoveAllObjectsAtOnce() {
+        let objects = testObjectsArray()
+        for object in objects {
+            container.append(object)
+        }
+        
+        container.removeAll()
+        
+        expect(self.container.weakItemsTable.count).to(equal(0))
+    }
+    
+    func testIfCanRetrieveObjects() {
+        let objectOne = NSObject()
+        let objectTwo = NSObject()
+        
+        container.append(objectOne)
+        container.append(objectTwo)
+        
+        let objects = container.items()
+        
+        expect(objects.count).to(equal(2))
+    }
+    
+    func testIfObjectsAddedToContainerCanBeDeallocated() {
+        var object:NSObject? = NSObject()
+        container.append(object!)
+        
+        object = nil
+        
+        let objects = container.items()
+        
+        expect(objects.count).to(equal(0))
+    }
+    
+    func testIfCanSeeObjectCount() {
+        let objects = testObjectsArray()
+        for object in objects {
+            container.append(object)
+        }
+
+        expect(self.container.count()).to(equal(objects.count))
+    }
+    
+}
+
+// MARK: Helper methods
+extension WeakItemContainerTests {
+    
+    func testObjectsArray() -> [NSObject] {
+        return [NSObject(), NSObject(), NSObject(), NSObject(), NSObject(), NSObject(), NSObject(), NSObject(), NSObject(), NSObject()]
     }
     
 }
